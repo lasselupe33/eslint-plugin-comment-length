d85fce2479a988fa068b4b71d44b32d63a3e247e
Lasse Felskov Agersten <lfa@appear.dk>
2021-09-15 12:52:40 +0200

    fix: include eslint/tslint/stylelint-enable comments into 'special' set

--- rules/src/rules/limit-multi-line-comments.ts
+++ rules/src/rules/limit-multi-line-comments.ts
@@ -177,7 +177,10 @@ function isSpecialComment(comment: Comment): boolean {
   return (
     comment.value.includes("eslint-disable") ||
     comment.value.includes("stylelint-disable") ||
-    comment.value.includes("tslint:disable")
+    comment.value.includes("tslint:disable") ||
+    comment.value.includes("eslint-enable") ||
+    comment.value.includes("stylelint-enable") ||
+    comment.value.includes("tslint:enable")
   );
 }
 
--- rules/src/rules/limit-single-line-comments.ts
+++ rules/src/rules/limit-single-line-comments.ts
@@ -168,6 +168,9 @@ function isSpecialComment(comment: Comment): boolean {
   return (
     comment.value.trim().startsWith("eslint-disable") ||
     comment.value.trim().startsWith("stylelint-disable") ||
-    comment.value.trim().startsWith("tslint:disable")
+    comment.value.trim().startsWith("tslint:disable") ||
+    comment.value.trim().startsWith("eslint-enable") ||
+    comment.value.trim().startsWith("stylelint-enable") ||
+    comment.value.trim().startsWith("tslint:enable")
   );
 }
